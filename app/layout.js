ec1bf089ed5ce1592b46834675f5c4ff8321ebd8
MauricioC12 <mauriciocontrerasr1202@gmail.com>
2024-06-14 00:55:23 -0500

    SEO and metada added

--- app/layout.js
+++ app/layout.js
@@ -9,12 +9,37 @@ const poppins = Poppins({
 export const metadata = {
   title: "Competec",
   description: "Somos Competec, empresa especializada en consultoría y asesoramiento en Tecnologías Cloud.",
-  keywords: ["TI", "Cloud", "Desarrollo Web", "Capacitacion"]
+  keywords: ["TI", "Cloud", "Desarrollo Web", "Capacitacion"],
+  openGraph: {
+    type: "website",
+    locale: "es_Es",
+    url: "https://www.competec.com.pe",
+    siteName: "Competec",
+    title: "Competec",
+    description: "Somos Competec, empresa especializada en consultoría y asesoramiento en Tecnologías Cloud.",
+    images: [
+      {
+        url: "https://www.competec.com.pe/Logo2.png",
+        width: 800,
+        height: 600,
+        alt: "Logo de Competec"
+      }
+    ]
+  }
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+        <link rel="manifest" href="/site.webmanifest"/>
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5"/>
+        <meta name="robots" content="index, follow" />
+      </head>
       <body className={`${poppins.className} dark:bg-[#1f1f22] bg-white`}>
         {children}
       </body>
---
+++ next-sitemap.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  siteUrl: 'https://www.competec.com.pe',
+  generateRobotsTxt: true, 
+};
