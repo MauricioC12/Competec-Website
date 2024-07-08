1441ab9914dbc95a2dd3363e314c9ada252c5cd2
MauricioC12 <mauriciocontrerasr1202@gmail.com>
2024-07-08 18:09:43 -0500

    Changes

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -2,26 +2,24 @@ import Link from "next/link";
 
 const Banner = () => {
   return (
-    <section className="py-24">
-      <div className="h-full px-6 lg:px-48">
-        <div className="relative flex items-center justify-center h-96 dark:text-white">
+    <section className="py-12 lg:py-24">
+      <div className="container mx-auto px-6 lg:px-48">
+        <div className="relative flex items-center justify-center h-full min-h-[50vh] lg:min-h-[96] dark:text-white">
           <div className="absolute inset-0 flex items-center justify-center">
-            <div className="relative w-full flex gap-8 lg:flex-row flex-col items-center justify-between">
-              <div className="relative w-full lg:w-2/3">
+            <div className="relative w-full flex flex-col lg:flex-row items-center justify-between lg:items-start">
+              <div className="w-full lg:w-1/2">
                 <img
                   src="/img/banner.png"
                   alt="Banner"
-                  className="relative z-10"
+                  className="mx-auto lg:mx-0"
                 />
               </div>
-              <div className="w-full lg:w-1/2 text-center lg:text-end">
-                <h1 className="text-4xl text-[#00274A] lg:text-6xl font-bold lg:leading-tight tracking-wider">
+              <div className="w-full lg:w-1/2 mb-8 lg:mb-0 lg:mr-8 text-center lg:text-right">
+                <h1 className="text-3xl lg:text-5xl font-bold lg:leading-tight tracking-wider">
                   Soluciones Que <br /> Transforman <br /> Tu Negocio
                 </h1>
-                <Link href="/contacto">
-                  <button className="mt-8 bg-[#005AAA] hover:bg-blue-500 text-white font-semibold py-3 px-6 rounded-lg">
-                    Contrata nuestros servicios
-                  </button>
+                <Link href="/contacto" className="mt-8 inline-block bg-[#005AAA] hover:bg-blue-500 text-white font-semibold py-3 px-6 rounded-lg">
+                    Cotiza nuestros servicios
                 </Link>
               </div>
             </div>
